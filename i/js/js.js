3dcd4cd359757257147cae50dca3d068d550aae3
judasn <jn3.141592654@gmail.com>
2013-07-27 17:23:56 +0800

    2013-07-27更新站点

--- i/js/js.js
+++ i/js/js.js
@@ -1,3 +1,21 @@
+ $(document).ready(function () {
+    /*标题提示效果 start*/
+    $(".updateNote").tipTip({defaultPosition: "top", maxWidth: "auto", edgeOffset: 2});
+    $(".updateNoteInput").tipTip({defaultPosition: "bottom", maxWidth: "auto", edgeOffset: 2, keepAlive: true});
+    /*标题提示效果 end*/
+});
+
+/*百度统计代码 start*/
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?cc19940e170de7f3eaddda5652ddbbca";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+/*百度统计代码 end*/
+
+ /*TAB切换效果 start*/
 function setTab(name, cursel, n) {
     for (i = 1; i <= n; i++) {
         var menu = document.getElementById(name + i);
@@ -6,3 +24,4 @@ function setTab(name, cursel, n) {
         con.style.display = i == cursel ? "block" : "none";
     }
 }
+ /*TAB切换效果 end*/
